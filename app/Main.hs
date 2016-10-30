bbc12799314a18d4b690181162c209689c330017
shinya takahashi <s.takahashi313@gmail.com>
2016-10-30 10:47:22 +0900

    ひながた

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
-import Lib
+import Web.Spock
+import Web.Spock.Config
+import Database.MySQL.Simple
+
+openDBConnection :: IO Connection
+openDBConnection = connect defaultConnectInfo { connectDatabase = "keijiban" }
+
+closeDBConnection :: Connection -> IO ()
+closeDBConnection = close
+
+dbConnectionBuilder :: ConnBuilder Connection
+dbConnectionBuilder = ConnBuilder openDBConnection closeDBConnection (PoolCfg 1 1 30)
+
+appCfg :: IO (SpockCfg Connection () ())
+appCfg = defaultSpockCfg () (PCConn dbConnectionBuilder) ()
+
+app :: SpockM Connection () () ()
+app = do
+    get root $
+      json "Hello world"
 
 main :: IO ()
-main = someFunc
+main = do
+    spockCfg <- appCfg
+    runSpock 8080 (spock spockCfg app)
