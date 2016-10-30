1d8be4ca9aa21f879f546f5036d1f6f113c12db6
shinya takahashi <s.takahashi313@gmail.com>
2016-10-30 12:30:56 +0900

    Added topics#create

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
 import Web.Spock
 import Web.Spock.Config
 import Database.MySQL.Simple
+import GHC.Generics (Generic)
+import Data.Aeson hiding (json)
+
+data RequestedTopic = RequestedTopic { val :: String } deriving (Generic, Show)
+data Topic = Topic { id :: Integer, name :: String } deriving (Generic, Show)
+
+instance FromJSON RequestedTopic
+instance ToJSON Topic
 
 openDBConnection :: IO Connection
 openDBConnection = connect defaultConnectInfo { connectDatabase = "keijiban" }
@@ -18,8 +28,12 @@ appCfg = defaultSpockCfg () (PCConn dbConnectionBuilder) ()
 
 app :: SpockM Connection () () ()
 app = do
-    get root $
-      json "Hello world"
+    post "topics" $ do
+      (RequestedTopic n) <- jsonBody'
+      runQuery $ \conn -> do
+        execute conn "INSERT INTO topics (name) VALUES (?)" [n]
+      json $ Topic 1 n
+
 
 main :: IO ()
 main = do
